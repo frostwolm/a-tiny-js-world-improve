4a70f326273b4f086e8387561f5c427469f34b13
Leonid Petrov <frostwolm@gmail.com>
2018-12-21 02:23:43 +0300

    add objects

--- index.js
+++ index.js
@@ -7,8 +7,51 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
-
-
+const dog = {
+  species:'dog',
+  name: 'Flow',
+  gender: 'female',
+  legs: 4,
+  hands: 0,
+  saying: 'woof-woof!',
+  friends:[cat, man]
+};
+const cat = {
+  species:'cat',
+  name: 'Mars',
+  gender: 'male',
+  legs: 4,
+  hands: 0,
+  saying: 'meow!',
+  friends:[dog, woman];
+};
+const man = {
+  species:'human',
+  name: 'Adrian',
+  gender: 'male',
+  legs: 2,
+  hands: 2,
+  saying: 'Good Morning, Vietnam!',
+  friends:[dog]
+};
+const woman = {
+  species:'human',
+  name: 'Trinh',
+  gender: 'female',
+  legs: 2,
+  hands: 2,
+  saying: 'Hin chao!',
+  friends:[cat]
+};
+const cat-woman = {
+  species:'human',
+  name: 'Kate',
+  gender: 'female',
+  legs: 2,
+  hands: 2,
+  saying: cat.saying,
+  friends:[]
+};
 // ======== OUTPUT ========
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
@@ -27,5 +70,3 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
-
-
