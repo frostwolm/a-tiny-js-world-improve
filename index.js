0feb0142615115661b2f20ca4151eb2f75d4a4f7
Leonid Petrov <frostwolm@gmail.com>
2019-01-14 02:33:30 +0300

    oop style

--- index.js
+++ index.js
@@ -1,91 +1,92 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
-
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
-
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-const dog = {
-  species:'dog',
-  name: 'Flow',
-  gender: 'female',
-  legs: 4,
-  hands: 0,
-  saying: 'woof-woof!',
-  friends:[]
-};
-const cat = {
-  species:'cat',
-  name: 'Mars',
-  gender: 'male',
-  legs: 4,
-  hands: 0,
-  saying: 'meow!',
-  friends:[]
-};
-const man = {
-  species:'human',
-  name: 'Adrian',
-  gender: 'male',
-  legs: 2,
-  hands: 2,
-  saying: 'Good Morning, Vietnam!',
-  friends:[]
-};
-const woman = {
-  species:'human',
-  name: 'Trinh',
-  gender: 'female',
-  legs: 2,
-  hands: 2,
-  saying: 'Hin chao!',
-  friends:[]
-};
-const catWoman = {
-  species:'human',
-  name: 'Kate',
-  gender: 'female',
-  legs: 2,
-  hands: 2,
-  saying: cat.saying,
-  friends:[]
-};
-
-dog.friends.push(cat, man);
-cat.friends.push(dog, catWoman);
-man.friends.push(dog, woman);
-woman.friends.push(man);
-catWoman.friends.push(cat);
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-function getStrFromObj(obj) {
-  return [
-    obj.species,
-    obj.name,
-    obj.gender,
-    obj.legs,
-    obj.hands,
-    obj.saying,
-    obj.friends.map(friend => friend.name).join(', ')
-  ].join('; ');
+class Inhabitant {
+  constructor(species, name, gender, saying, legs, hands) {
+    this.species = species;
+    this.name = name;
+    this.gender = gender;
+    this.saying = saying;
+    this.legs = legs;
+    this.hands = hands;
+    this.friends = [];
+  }
+  addFriend(friend){
+    this.friends.push(friend);
+    friend.friends.push(this);
+    return this;
+  }
+  toString(){
+    return [this.species, this.name, this.gender, this.legs, this.hands, this.saying, this.friends.map(friend => friend.name).join(', ')].join ('; ') + '; ';
+  }
 }
 
-[dog, cat, man, woman, catWoman].forEach((hab) => print(getStrFromObj(hab)));
+class Human extends Inhabitant {
+  constructor(species, name, gender, saying, legs = 2, hands = 2) {
+    super(species, name, gender, saying, legs, hands);
+    this.hands = hands;
+  }
+}
+
+class Animal extends Inhabitant {
+  constructor(species, name, gender, saying, legs = 4, hands = 0) {
+    super(species, name, gender, saying, legs, hands);
+  }
+}
+
+class Man extends Human {
+  constructor(name, saying) {
+      super('human', name, 'male', saying);
+  }
+}
+
+class Woman extends Human {
+  constructor(name, saying) {
+      super('human', name, 'female', saying);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, gender, saying) {
+      super('cat', name, gender, saying);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, gender, saying) {
+      super('dog', name, gender, saying);
+  }
+}
+
+class CatWoman extends Human {
+  constructor(name, saying) {
+      super('human', name, 'female', saying);
+  }
+}
+
+class World {
+  constructor() {
+    this.habbitants = Array.from(arguments);
+  }
+  addHabbitant(hab){
+    this.habbitants.push(hab);
+    return this;
+  }
+  toString(){
+    return this.habbitants.map(hab => print(hab)).join('\n');
+  }
+}
+
+const dog = new Dog('Flow', 'female', 'woof-woof!');
+const cat = new Cat('Mars', 'male', 'meow!');
+const man = new Man('Adrian', 'Good Morning, Vietnam!');
+const woman = new Woman('Trish', 'Hin chao!');
+
+const world = new World(dog, cat, man, woman);
+
+dog.addFriend(cat).addFriend(man);
+man.addFriend(woman);
+
+const catWoman = new CatWoman('Kate', cat.saying);
+catWoman.addFriend(cat);
+
+world.addHabbitant(catWoman);
+
+print(world);
